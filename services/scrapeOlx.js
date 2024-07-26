c12814119e23d6e4311b4cecabb60557b5ba8224
Arlan Marat <arlankill12@gmail.com>
2024-07-26 16:18:39 +0500

    Add

--- services/scrapeOlx.js
+++ services/scrapeOlx.js
@@ -7,7 +7,7 @@ const scrapeProductPageOlx = async (url, query) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: process.env.CHROMIUM_PATH,
+    executablePath: '/usr/bin/chromium-browser'
   });
   const page = await browser.newPage();
 
@@ -45,7 +45,7 @@ const scrapeOlx = async (query) => {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    executablePath: process.env.CHROMIUM_PATH,
+    executablePath: '/usr/bin/chromium-browser'
   });
   const page = await browser.newPage();
 
